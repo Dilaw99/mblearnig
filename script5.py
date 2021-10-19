d96aaa334bb22054b60c9461c9e789c90185d457
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-10-19 08:20:27 +0100

    Fix loop iteration

--- script5.py
+++ script5.py
@@ -1,10 +1,6 @@
-var1=0
 var2=20
-for i in range(1,var2+1) :
-    var1=var1+1
-    print(int(var1))
-
-var1=0
-for i in range(1,var2-9) :
-    var1=var1+1
+for i in range(1,var2) :
+    print(int(i))
+print('---------------------------')
+for i in range(1,var2, 2) :
     print(int(var1))
