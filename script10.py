8755f082ae8c6d8ac011a081dce30b13c99049c9
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-10-19 08:25:08 +0100

    Change tpl from list to tuple & fix return statement in function

--- script10.py
+++ script10.py
@@ -1,9 +1,9 @@
 
-def somme2(tuple_de_longeur_variable) :
+def somme2(var) :
     s2=0
-    for i in range(0,len(tpl)) :
-        s2=s2+tuple_de_longeur_variable[i]
-        print(s2)
+    for i in range(0,len(var)) :
+        s2=s2+var[i]
+    return s2
 
-tpl=[1,2,3,4,5,6,7,8,9,10]
-somme2(tpl)
+tpl=(1,2,3,4,5,6,7,8,9,10)
+print(somme2(tpl))
